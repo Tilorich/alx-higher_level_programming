27135c9ad576e8ff57006523101d41b28b28cfd6
Tilorich <mbarichard18@gmail.com>
2022-10-06 05:27:51 -0700

    last digit guessing file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = str(number)
-figure = int(number[-1])
+figure = abs(number) % 10
+
+if number < 0:
+    figure = -figure
+
 if figure > 5:
     print(f"Last digit of {number} is {figure} and is greater than 5")
 elif figure < 6:
     if figure == 0:
-      print(f"Last digit of {number} is {figure} and is 0")
+        print(f"Last digit of {number} is {figure} and is 0")
     else:
-      print(f"Last digit of {number} is {figure} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {figure} and is less than 6 and not 0")
