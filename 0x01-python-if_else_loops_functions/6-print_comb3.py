dc817a697298dd6d105785df45167dd358868c5e
Tilorich <mbarichard18@gmail.com>
2022-10-06 07:22:32 -0700

    number manipulation file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 for number in range(100):
-    if number == 89:
-        print(number)
-    if (number // 10) == (number % 10):
+    if ((number // 10) == 8) and ((number % 10) == 9):
+        print("{}".format(number))
+    elif (number // 10) == (number % 10):
         continue
     elif (number // 10) > (number % 10):
         continue
-    print("{:0>2}, ".format(number), end='')
+    else:
+        print("{:0>2}, ".format(number), end='')
