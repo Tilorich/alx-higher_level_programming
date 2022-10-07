3895a4444bf1eee82dfe6e4118edba5726a83aad
Tilorich <mbarichard18@gmail.com>
2022-10-07 13:24:21 -0700

    python argv file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     if length == 0:
         print("{} arguments.".format(0))
     elif length == 1:
-        print("{} argument:".format(length)
-        print("{}: {}".format(length, arg[1])
+        print("{} argument:".format(length))
+        print("{}: {}".format(length, arg[1]))
     else:
         print("{} arguments:".format(length))
-        for i in range(len(arg)):
+        for i in range(length):
             print("{}: {}".format(i + 1, arg[i + 1]))
