d4b7aa44ac1a4e412266665a47d08c6d111951a4
Tilorich <mbarichard18@gmail.com>
2022-10-12 11:39:42 -0700

    list manipulation

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[])
     list_result = []
     for i in range(len(my_list)):
-        if mylist[i] % 2 == 0:
+        if my_list[i] % 2 == 0:
             list_result.append(True)
         else:
             list_result.append(False)
