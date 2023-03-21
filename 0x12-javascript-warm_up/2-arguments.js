c9a233e1d65adb6797336a7c57a3b9860d37722a
Tilorich <mbarichard18@gmail.com>
2023-03-21 12:46:09 +0000

    how to use argument in JS

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const arg = process.argv.lenght;
-console.log(arg === 2 ? 'No argument' : arg === 3 ? 'Argument found' : 'Argument found');
+console.log(arg === 2 ? 'No argument' : arg === 3 ? 'Argument found' : 'Arguments found');
