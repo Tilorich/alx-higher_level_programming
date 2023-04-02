23eb9ad8d4319d6bf3c6cfb555c2b4ee5783aeb9
Tilorich <mbarichard18@gmail.com>
2023-04-02 15:27:06 -0700

    project files update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
-"""
+"""fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
 
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+if __name__ == "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
